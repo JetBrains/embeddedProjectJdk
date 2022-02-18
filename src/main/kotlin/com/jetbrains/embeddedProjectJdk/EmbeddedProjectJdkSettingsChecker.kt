a76f36b21d57d4e7f03b0ec10e1ee9823ea17d39
Mikhail Filippov <mikhail@filippov.me>
2022-02-18 10:18:36 +0300

    Migrate to new API.

--- src/main/kotlin/com/jetbrains/embeddedProjectJdk/EmbeddedProjectJdkSettingsChecker.kt
+++ src/main/kotlin/com/jetbrains/embeddedProjectJdk/EmbeddedProjectJdkSettingsChecker.kt
@@ -2,18 +2,15 @@ package com.jetbrains.embeddedProjectJdk
 
 import com.intellij.notification.Notification
 import com.intellij.notification.NotificationAction
-import com.intellij.notification.NotificationGroup
+import com.intellij.notification.NotificationType
 import com.intellij.openapi.actionSystem.ActionManager
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.StartupActivity
-import com.intellij.openapi.ui.MessageType
 
 class EmbeddedProjectJdkSettingsChecker : StartupActivity {
   companion object {
-    val PER_PROJECT_JDK_SETTINGS =
-      NotificationGroup.toolWindowGroup("Per Project JDK Settings", "PerProjectJdkSettings")
     const val LOAD_SETTINGS_ACTION_ID = "LoadJdkSettingsFromProject"
   }
 
@@ -22,14 +19,13 @@ class EmbeddedProjectJdkSettingsChecker : StartupActivity {
   override fun runActivity(project: Project) {
     myLogger.debug("Handle before project loaded event")
     if (JdkUtil.hasDifferentJdkSettings(project)) {
-      PER_PROJECT_JDK_SETTINGS
-        .createNotification(Messages.message("notification.text"), MessageType.WARNING)
+      Notification("Per Project JDK Settings", Messages.message("notification.text"), "", NotificationType.WARNING)
         .addAction(object : NotificationAction(Messages.message("notification.action.text")) {
-          override fun actionPerformed(e: AnActionEvent, notification: Notification) {
-            ActionManager.getInstance().getAction(LOAD_SETTINGS_ACTION_ID).actionPerformed(e)
-            notification.expire()
-          }
-        }).notify(project)
+        override fun actionPerformed(e: AnActionEvent, notification: Notification) {
+          ActionManager.getInstance().getAction(LOAD_SETTINGS_ACTION_ID).actionPerformed(e)
+          notification.expire()
+        }
+      }).notify(project)
     }
   }
 }
